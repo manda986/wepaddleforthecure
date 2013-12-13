f0aa44b2444e2e794987ecbb252b883196e81d99
Amanda Li <manda986@gmail.com>
2013-12-12 22:00:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wepaddleforthecure::Application.config.secret_key_base = '49204e1fe23b79276b3318f1d0c1efa05071f9ad4a3202e6b8e6eecf8c5718dc40feafcaa0e7c07500021d5b807d21f1ac2d903377a34806634a17f9ebbb438c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wepaddleforthecure::Application.config.session_store :cookie_store, key: '_wepaddleforthecure_session'
